fixed the normalization

drugmarket/neuralnetwork_predict.py
@@ -9,14 +9,12 @@ import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from process_forlogisticregression import get_data
 
-
 def y2indicator(y, K):
     N = len(y)
     ind = np.zeros((N, K))
     for i in range(N):
         ind[i, y[i]] = 1
     return ind
-
 
 Xtrain, Ytrain, Xtest, Ytest = get_data()
 D = Xtrain.shape[1]
@@ -34,15 +32,17 @@ W2 = np.random.randn(M, K)
 b2 = np.zeros(K)
 
 # make predictions
-
-
 def softmax(a):
     expA = np.exp(a)
     return expA / expA.sum(axis=1, keepdims=True)
 
+def sigmoid(a):
+    return 1 / (1 + np.exp(-a))
+
 def forward(X, W1, b1, W2, b2):
     Z = np.tanh(X.dot(W1) + b1)
-    return softmax(Z.dot(W2) + b2), Z
+    # return softmax(Z.dot(W2) + b2), Z
+    return sigmoid(Z.dot(W2) + b2), Z # doing binary classification so can use sigmoid
 
 def predict(P_Y_given_X):
     return np.argmax(P_Y_given_X, axis=1)
@@ -53,7 +53,6 @@ def classification_rate(Y, P):
 
 def cross_entropy(T, pY):
     return -np.mean(T * np.log(pY))
-
 
 # train loop
 train_costs = []
@@ -75,6 +74,7 @@ for i in range(10000):
     W1 -= learning_rate * Xtrain.T.dot(dZ)
     b1 -= learning_rate * dZ.sum(axis=0)
     if i % 1000 == 0:
+        print(pYtrain)
         print(i, ctrain, ctest)
 
 print("Final train classification_rate:",

drugmarket/process_forlogisticregression.py
@@ -16,6 +16,7 @@ def get_data():
     # remove outliers
     df = df[df['MC'] > 0]
     df = df[ (df['Phase 4'] > 1) | (df['Phase 3'] > 1) | (df['Phase 2'] > 1) | (df['Phase 1'] > 1)] # has any trials
+    df = df[ (df['Phase 4'] < 500) | (df['Phase 3'] < 500) | (df['Phase 2'] < 500) | (df['Phase 1'] < 500)] # has too many trials
     df = df[df['Symbol'] != "GILD"]
     df = df[df['Symbol'] != "SYK"]
     df = df[df['Symbol'] != "MDT"]
@@ -34,7 +35,6 @@ def get_data():
 
     # split features and labels
     X = data[:, 3:-2].astype(np.int32) # this just pulled excluded the last two columns
-
     Y = data[:, -1].astype(np.int32) # this is the last column
 
     # one-hot encode the categorical data
@@ -52,19 +52,18 @@ def get_data():
     # assign X2 back to X, since we don't need original anymore
     # X = X2
 
-    # split train and test
+    # split train and test, then convert to floats for normalization
     # has total 164 rows
-    Xtrain = X[:-50]
-    Ytrain = Y[:-50]
-    Xtest = X[-50:]
-    Ytest = Y[-50:]
+    Xtrain = X[:-120].astype(np.float32)
+    Ytrain = Y[:-120]
+    Xtest = X[-120:].astype(np.float32)
+    Ytest = Y[-120:]
 
-    # normalize phase columns
-    # Not doing this because some are so high, it ends outputting many zeros
-    # for i in (0, 1, 2, 3):
-    #     m = Xtrain[:, i].mean()
-    #     s = Xtrain[:, i].std()
-    #     Xtrain[:, i] = (Xtrain[:, i] - m) / s
-    #     Xtest[:, i] = (Xtest[:, i] - m) / s
+    # normalize phase columns by X - mean / std
+    for i in (0, 1, 2, 3):
+        m = Xtrain[:, i].mean()
+        s = Xtrain[:, i].std()
+        Xtrain[:, i] = (Xtrain[:, i] - m) / s
+        Xtest[:, i] = (Xtest[:, i] - m) / s
 
     return Xtrain, Ytrain, Xtest, Ytest